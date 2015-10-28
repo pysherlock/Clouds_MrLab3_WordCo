32816ce8e2f62c0e88b984bcec21d25fb963fa96
pu <yang.pu@eurecom.fr>
2015-10-29 00:27:30 +0100

    Ex3_Begin

---
+++ src/main/java/fr/eurecom/dsg/mapreduce/App.java
@@ -0,0 +1,13 @@
+package fr.eurecom.dsg.mapreduce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
